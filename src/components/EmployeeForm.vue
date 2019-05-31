f75212bcd2e8e2839e6ba4da678543ca2fc72866
Rich Keyzor <richkeyzor@Richs-Mac-mini.home>
2019-05-31 10:39:51 +0100

    Add Employee Form

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="employee-form">
+        <form action="">
+            <label for="">Employee name</label>
+            <input type="text">
+            <label for="">Employee email</label>
+            <input type="text">
+            <button>Add Employee</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'employee-form',
+    data() {
+        return {
+            employee: {
+                name: '',
+                email: ''
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        margin-bottom: 2rem;
+    }
+</style>
