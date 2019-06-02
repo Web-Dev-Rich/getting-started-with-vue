fce028c7c942e97e0bc924e59353f51e6174ee0a
Rich Keyzor <web_dev_rich@keyzor.net>
2019-06-02 09:36:34 +0100

    Add CSS for error checks

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -63,7 +63,19 @@ export default {
 </script>
 
 <style scoped>
-    form {
-        margin-bottom: 2rem;
-    }
+  form {
+      margin-bottom: 2rem;
+  }
+  
+  [class*='-message'] {
+    font-weight: 500;
+  }
+
+  .error-message {
+    color: #d33c40;
+  }
+
+  .success-message {
+    color: #32a95d;
+  }
 </style>
